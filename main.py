a9d21331531911fa7ed6dff06a363a613cddd177
szymonjuraszek <szymonjuraszek777@gmail.com>
2019-12-08 12:53:37 +0100

    Initia commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Dany jest obraz kolorowy na którym widocznych jest wiele obiektów o tym samym kształcie
+# i wielkości ułożonych pod różnym kątem. Znajdź rozkład orientacji obiektów i policz ile ich jest na obrazie.
+
+
+print('Sajmon')
